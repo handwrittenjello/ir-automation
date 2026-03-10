ec3a9231040e3fc414958a2b2a5518ea83e9ea04
Drew Littlejohn <handwrittenjello@gmail.com>
2026-03-10 16:39:39 -0400

    Adding sleep

--- tv_on.py
+++ tv_on.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+import time
 import config
 from flipper_serial import send_ir_command
 
+send_ir_command(config.IR_TV_POWER, config.FLIPPER_PORT, config.FLIPPER_BAUD)
+time.sleep(2)
 send_ir_command(config.IR_TV_POWER, config.FLIPPER_PORT, config.FLIPPER_BAUD)
